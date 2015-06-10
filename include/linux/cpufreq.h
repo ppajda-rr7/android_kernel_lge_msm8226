75df194e1367e34b1b41326e168d6b0effec87f9
GrinningFerret <luis.hage@outlook.de>
2015-06-10 13:46:57 +0200

    CPU: New interactivex governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -384,7 +384,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_pegasusq)
 #endif
-
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX	)
+extern struct cpufreq_governor cpufreq_gov_interactivex;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_interactivex)
 
 /*********************************************************************
  *                     FREQUENCY TABLE HELPERS                       *
