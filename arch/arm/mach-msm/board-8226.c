886f993cfc98839bfe5490f453ae57a7238c3c4e
GrinningFerret <luis.hage@outlook.de>
2015-08-13 14:48:11 +0200

    Revert "intellithermal v2: adapt for MSM8226 architecture"
    This reverts commit 405d54f611be239487bde1ba206a3845990b7040.

--- arch/arm/mach-msm/board-8226.c
+++ arch/arm/mach-msm/board-8226.c
@@ -145,11 +145,7 @@ void __init msm8226_add_drivers(void)
 	fan53555_regulator_init();
 	cpr_regulator_init();
 	tsens_tm_init_driver();
-#ifdef CONFIG_INTELLI_THERMAL
-	msm_thermal_init(NULL);
-#else
 	msm_thermal_device_init();
-#endif
 }
 
 void __init msm8226_init(void)
